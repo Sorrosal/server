52d2fd6bf12c987c622d642b6579c31d9ab5f1ba
Sorrosal <sergiosorrosalgayan@gmail.com>
2023-04-13 17:50:08 +0200

    Estructura y autenticación de la api

---
+++ constants.js
@@ -0,0 +1,17 @@
+const DB_USER = "admin";
+const DB_PASSWORD = "admin";
+const DB_HOST = "web-personal.uujujyy.mongodb.net";
+
+const API_VERSION = "v1";
+const IP_SERVER = "localhost";
+
+const JWT_SECRET_KEY = "rgujnbowlfmnd78fh";
+
+module.exports = {
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    API_VERSION,
+    IP_SERVER,
+    JWT_SECRET_KEY,
+};
---
+++ index.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const app = require("./app")
+const {DB_USER,DB_PASSWORD,DB_HOST,IP_SERVER,API_VERSION} = require("./constants");
+
+const PORT = process.env.POST || 3977;
+
+mongoose.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`, 
+    (error) => {
+        if(error) throw error;
+
+        app.listen(PORT, () => {
+            console.log(`El servidor se ha levantado en  http://${IP_SERVER}:${PORT}/api/${API_VERSION}`)
+        })
+    }
+)
